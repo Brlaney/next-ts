d32a2e6e89f22c762794f0873e7868981d112d05
brlaney42 <brlaney@outlook.com>
2022-02-17 19:42:21 -0600

    Updates and added carousel

--- src/components/Images.tsx
+++ src/components/Images.tsx
@@ -1,24 +1,23 @@
-import image1 from '/public/images/1.jpg';
-import image2 from '/public/images/2.jpg';
-import image3 from '/public/images/3.jpg';
-import image4 from '/public/images/4.jpg';
-import image5 from '/public/images/5.jpg';
-import image6 from '/public/images/6.jpg';
-import image7 from '/public/images/7.jpg';
-import image8 from '/public/images/8.jpg';
-import image9 from '/public/images/9.jpg';
-import image10 from '/public/images/10.jpg';
-import image11 from '/public/images/11.jpg';
-import image12 from '/public/images/12.jpg';
-import image13 from '/public/images/13.jpg';
-import image14 from '/public/images/14.jpg';
-import image15 from '/public/images/15.jpg';
-import image16 from '/public/images/16.jpg';
-import image17 from '/public/images/17.jpg';
-import image18 from '/public/images/18.jpg';
-import image19 from '/public/images/19.jpg';
-import image20 from '/public/images/20.jpg';
-import image21 from '/public/images/21.jpg';
+import image1 from '/public/images/Optimized-1.jpg';
+import image2 from '/public/images/Optimized-2.jpg';
+import image3 from '/public/images/Optimized-3.jpg';
+import image4 from '/public/images/Optimized-4.jpg';
+import image5 from '/public/images/Optimized-5.jpg';
+import image6 from '/public/images/Optimized-6.jpg';
+import image7 from '/public/images/Optimized-7.jpg';
+import image8 from '/public/images/Optimized-8.jpg';
+import image9 from '/public/images/Optimized-9.jpg';
+import image10 from '/public/images/Optimized-10.jpg';
+import image11 from '/public/images/Optimized-11.jpg';
+import image12 from '/public/images/Optimized-12.jpg';
+import image13 from '/public/images/Optimized-13.jpg';
+import image14 from '/public/images/Optimized-14.jpg';
+import image15 from '/public/images/Optimized-15.jpg';
+import image16 from '/public/images/Optimized-16.jpg';
+import image17 from '/public/images/Optimized-17.jpg';
+import image18 from '/public/images/Optimized-18.jpg';
+import image19 from '/public/images/Optimized-19.jpg';
+import image20 from '/public/images/Optimized-20.jpg';
 
 export default [
   image1,
@@ -40,6 +39,5 @@ export default [
   image17,
   image18,
   image19,
-  image20,
-  image21
+  image20
 ];
--- src/pages/carousel/index.tsx
+++ src/pages/carousel/index.tsx
@@ -1,12 +1,40 @@
+import { useRef, useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
-import styles from '@/styles/pages/Carousel.module.scss';
 import images from '@/components/Images';
+import Image from 'next/image'
+import styles from '@/styles/pages/Carousel.module.scss';
+
 
 const Carousel = () => {
+  const [width, setWidth] = useState(0);
+  const carousel = useRef();
+
+  useEffect(() => {
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+  }, []);
+
   return (
     <motion.div className={styles.container}>
-      <motion.div className={styles.grid}>
-        Carousel page
+      <motion.div ref={carousel} className={styles.carousel}>
+        <motion.div
+          drag='x'
+          dragConstraints={{ right: 6158, left: -6158 }}
+          className={styles.innerCarousel}
+        >
+
+          {/* Iterate over your array of images */}
+          {images.map((image, i: number) => {
+            return (
+              <motion.div className={styles.item} key={i}>
+                <Image
+                  layout='responsive'
+                  src={image}
+                  alt=''
+                />
+              </motion.div>
+            )
+          })}
+        </motion.div>
       </motion.div>
     </motion.div>
   )
