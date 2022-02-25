4a7046abae5960d6942ba9864dd0a89b90bccec7
brlaney <brlaney@outlook.com>
2022-02-24 20:33:01 -0600

    Pushing changes

---
+++ src/lib/utils/math/reynolds.ts
@@ -0,0 +1,40 @@
+// reynolds.ts
+
+export function Re1(p, u, mu, L) {
+  // Reynolds number using eqn 1
+  let initial = p * u * L / mu;
+
+  // Rounded to 5 decimal places
+  let Re = initial.toFixed(6);
+
+  return Re;
+};
+
+export function Re2(u, L, v) {
+  // Reynolds number using eqn 2
+  let initial = u * L / v;
+
+  // Rounded to 5 decimal places
+  let Re = initial.toFixed(6);
+
+  return Re;
+};
+
+/*
+  Re = ρ u L / μ              (eqn 1)
+     = ρ u2 / (μ u / L) 
+     = u L / ν                (eqn 2)                                         
+ 
+  where:
+    Re = Reynolds Number (non-dimensional)   (1)
+    ρ = density (kg/m3, lbm/ft3)
+    u = velocity based on the actual cross section area of the duct or pipe (m/s, ft/s)
+    μ = dynamic viscosity (Ns/m2, lbm/s ft)
+    L = characteristic length (m, ft)
+    ν = μ / ρ = kinematic viscosity (m2/s, ft2/s)
+
+  laminar - when Re < 2300
+  transient - when 2300 < Re < 4000
+  turbulent - when Re > 4000
+
+*/
--- src/pages/moody/index.tsx
+++ src/pages/moody/index.tsx
@@ -2,18 +2,23 @@
 import { motion } from 'framer-motion';
 // import images from '@/components/Images';
 // import Image from 'next/image';
+import { Re1, Re2 } from '@/lib/utils/reynolds';
 import styles from '@/styles/pages/Moody.module.scss';
 
 
 const Moody = () => {
-//   useEffect(() => {
-//   }, []);
+  // Re1(p, u, mu, L)
+  // Re2(u, L, v)
+
+  // Test function 1:
+  const test1 = Re1(910, 2.6, 0.025, 0.38)
+  console.log(test1)
 
   return (
     <motion.div className={styles.container}>
       <motion.div className={styles.grid}>
         <motion.div className={styles.moody}>
-            Moody Diagram Here
+            {test1}
         </motion.div>
       </motion.div>
     </motion.div>
