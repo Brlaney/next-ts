c7ff31322f9bc8e9bd2199658fea4256eb5447e5
brlaney42 <brlaney@outlook.com>
2022-03-18 15:08:20 -0500

    Rem more unused files

--- src/lib/data/game/index.ts
+++
@@ -1,7 +0,0 @@
-import { IVials } from '@/lib/types';
-
-export const vials: IVials[] = [
-  { id: 1, slots: [1, 2, 1, 1, 2], status: [5, 0] },
-  { id: 2, slots: [2, 1, 2, 1, 2], status: [5, 0] },
-  { id: 3, slots: [0, 0, 0, 0, 0], status: [0, 5] },
-];
